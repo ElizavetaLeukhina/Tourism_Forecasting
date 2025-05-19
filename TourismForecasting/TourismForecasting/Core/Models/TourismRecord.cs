b085b4e77c84418d25e0126d07f8a17cde91de5a
Leukhina Elizaveta <leukhinaelizaveta@gmail.com>
2025-05-19 22:20:35 +0300

    Добавлена модель данных TourismRecord

---
+++ TourismForecasting/TourismForecasting/Core/Models/TourismRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourismForecastingApp.Core.Models
+{
+    public class TourismRecord
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public int Tourists { get; set; }
+    }
+}
