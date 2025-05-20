f00ae96c59caafb62b098f517b8a105cb93fec78
Leukhina Elizaveta <leukhinaelizaveta@gmail.com>
2025-05-20 12:07:07 +0300

    Добавлена базовая загрузка данных из JSON

---
+++ TourismForecasting/TourismForecasting/Core/Services/TourismDataLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TourismForecastingApp.Core.Models;
+
+namespace TourismForecastingApp.Core.Services
+{
+    public static class TourismDataLoader
+    {
+        public static List<TourismRecord> LoadFromJson(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            var records = JsonConvert.DeserializeObject<List<TourismRecord>>(json);
+            return records ?? new List<TourismRecord>();
+        }
+    }
+}
