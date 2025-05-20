210055f46e9ded41211a4cb61ab1ec408c7998c8
Leukhina Elizaveta <leukhinaelizaveta@gmail.com>
2025-05-20 12:38:11 +0300

    Добавлена проверка на существование файла

--- TourismForecasting/TourismForecasting/Core/Services/TourismDataLoader.cs
+++ TourismForecasting/TourismForecasting/Core/Services/TourismDataLoader.cs
@@ -10,6 +10,9 @@ namespace TourismForecastingApp.Core.Services
     {
         public static List<TourismRecord> LoadFromJson(string filePath)
         {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"Файл не найден: {filePath}");
+
             string json = File.ReadAllText(filePath);
             var records = JsonConvert.DeserializeObject<List<TourismRecord>>(json);
             return records ?? new List<TourismRecord>();
